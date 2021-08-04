b3bcca4c98c7e98dba492149bfc127ea60d9ec29
Ted Tibbetts <ted@circulated.ca>
2021-08-03 22:05:55 -0300

    Price lookup now checks all printings.

--- __init__.py
+++ __init__.py
@@ -12,6 +12,14 @@ import pprint
 from functools import partial
 pp = partial(pprint.pprint, sort_dicts=False)
 
+class SFException(Exception):
+    pass
+
+debug = print
+debug = lambda *args, **kwargs: None
+
+#debug = lambda *args, **kwargs: print(*args, **kwargs)
+
 class Fetcher:
     last_request = 0      # timestamp of last retrieval
     min_interval = 100000 # minimum interval between requests in ns
@@ -19,17 +27,54 @@ class Fetcher:
     api_url = 'https://api.scryfall.com/cards/named'
 
     def get(self, card_name, timeout=2.0):
-        """Fetch pricing data for card `card_name`."""
-        j = self.get_card(card_name, timeout=timeout)
+        """Fetch pricing data for card `card_name`.
+
+        Retrieves data for all printings with that exact name (case insensitive).
+
+        """
+        j = self.find_card_name(card_name, timeout=timeout)
         return j['prices']['usd']
 
     def get_card(self, card_name, timeout=2.0):
-        """Fetch info for card `card_name`
+        """Fetch info for card `card_name` using the API name url.
 
         Returns full parsed JSON for the card name.
         """
         r = self.request(self.api_url, {'exact': card_name}, timeout=timeout)
         return json.loads(r.text)
+
+    def search_card_name(self, card_name, timeout=2.0):
+        """Search via the scryfall API for all printings of an exact card name.
+
+        Returns English results only.
+
+        Results are sorted by USD value from high to null.
+        """
+        r = self.request('https://api.scryfall.com/cards/search',
+                         {'unique': 'prints',
+                          'order': 'usd',
+                          'q': f'lang:en !"{card_name}"'},
+                         timeout=timeout)
+        j = json.loads(r.text)
+        if 'data' not in j:
+            raise SFException(f'Invalid card name "{card_name}".')
+
+        return j['data']
+
+    def find_card_name(self, card_name, timeout=2.0):
+        """Calls `search_card_name` and trims results to cheapest valid printing.
+
+        Filters out any results for which 'set_type' is 'promo' or for which there is no usd price.
+
+        Returns the last (lowest-priced) result.
+        """
+        printings = self.search_card_name(card_name, timeout=timeout)
+
+        printings = [card for card in printings
+                     if card['set_type'] != 'promo' and card['prices']['usd'] is not None]
+        if not printings:
+            raise SFException(f'No valid results for card name "{card_name}"')
+        return printings[-1]
 
     def request(self, url, params, timeout=2.0):
         """Make a request from the scryfall REST API.
@@ -47,6 +92,8 @@ class Fetcher:
             time.sleep((now - self.last_request) / 1000000.0)
 
         self.last_request = now
-        return requests.get(url, params, timeout=timeout)
+        r = requests.get(url, params, timeout=timeout)
+        debug(f'Fetcher.request called.  url: {r.url}')
+        return r
 
 fetcher = Fetcher() # singleton object to restrict retrieval frequency
--- __main__.py
+++ __main__.py
@@ -8,10 +8,17 @@ def print_price(card_name):
 def print_card(card_name):
     pp(fetcher.get_card(card_name))
 
+def search_card(card_name):
+    pp(fetcher.search_card_name(card_name))
+
 argparser = argparse.ArgumentParser(description='Card price fetcher for scryfall.com')
 argparser.add_argument('-c', '--card', dest='action', action='store_const',
                        const=print_card, default=print_price,
                        help='Print full card data instead of just the price.')
+argparser.add_argument('-s', '--search', dest='action', action='store_const',
+                       const=search_card, default=print_price,
+                       help='Search for the card name and return all unique printings.')
+
 argparser.add_argument('card_name', type=str)
 args = argparser.parse_args()
 
